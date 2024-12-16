fbe76ec440d8278f19c80b29c80401796c89e8aa
Léo Mendes <113518053+LeoMendesEsEtml@users.noreply.github.com>
2024-12-16 08:01:56 +0100

    Ex 5 last commit

--- Exercices/Ex5_RevisionClass1_POO/Ex5.cpp
+++ Exercices/Ex5_RevisionClass1_POO/Ex5.cpp
@@ -9,8 +9,27 @@ using namespace std;
 int main(void)
 {
 	char Rep;
-	
+	Dessinateur DessinateurA, DessinateurB, DessinateurC;
 	// Déclaration et création des dessinateurs
+	
+	DessinateurA.ConstruireFigure('L');
+	DessinateurB.ConstruireFigure('R');
+	DessinateurC.ConstruireFigure('E');
+
+
+	DessinateurA.SaisirPositionXY(1);
+	DessinateurA.SaisirPositionXY(2);
+
+	DessinateurB.SaisirPositionXY(1);
+	DessinateurB.SaisirPositionXY(2);
+
+	DessinateurC.SaisirPositionXY(1);
+	DessinateurC.SaisirPositionXY(2);
+
+	DessinateurA.DessinerFigure();
+	DessinateurB.DessinerFigure();
+	DessinateurC.DessinerFigure();
+
 	
 
 	cout << "Cours SL228_POBJ CPP exercice 5" << endl;
