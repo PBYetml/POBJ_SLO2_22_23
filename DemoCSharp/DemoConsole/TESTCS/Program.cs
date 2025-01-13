be6422cb0727cb9835a55d2d820d8e4822d9a635
Léo Mendes <113518053+LeoMendesEsEtml@users.noreply.github.com>
2025-01-13 10:16:04 +0100

    new ex cs

---
+++ DemoCSharp/DemoConsole/TESTCS/Program.cs
@@ -0,0 +1,25 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+// attributs
+// Type primaire 
+
+//famille Entier
+bool demoBool;
+
+
+char demoChar;
+
+short demoShort;
+ushort demoUShort;
+
+int demoInt;
+uint demoUInt;
+
+long demoLong;  
+
+
+
+ulong demoULong;
+
+// Méthode
